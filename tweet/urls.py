a6eee94f77f5cbae95fc011fb3a23eef9ef26aef
DESKTOP-REGETCI\SinYoung <uky00832@gmail.com>
2022-05-27 20:42:45 +0900

    게시물 보여주기, 삭제 기능 추가

--- tweet/urls.py
+++ tweet/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home'), # 127.0.0.1:8000과 views.py 폴더의 home 함수 연결
-    path('tweet/', views.tweet, name = 'tweet')
+    path('', views.home, name='home'), # 127.0.0.1:8000과 views.py 폴더의 home 함수 연결
+    path('tweet/', views.tweet, name='tweet'),
+    path('tweet/delete/<int:id>', views.delete_tweet, name='delete-tweet'),
 ]
--- tweet/views.py
+++ tweet/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import TweetModel
+from django.contrib.auth.decorators import login_required # 로그인되어 있어야만 함수가 발동하게 함
+
 
 # Create your views here.
 def home(request):
@@ -9,11 +11,13 @@ def home(request):
     else:
         return redirect('/sign-in')
 
+
 def tweet(request):
     if request.method == 'GET':
         user = request.user.is_authenticated  # 사용자가 로그인했는지 인증함
         if user:
-            return render(request, 'tweet/home.html')
+            all_tweet = TweetModel.objects.all().order_by('-created_at') # order_by 생성된 시간을 역순으로 불러옴(역순으로 정렬하도록 - 를 붙여줌)
+            return render(request, 'tweet/home.html', {'tweet': all_tweet})
         else:
             return redirect('/sign-in')
     elif request.method == 'POST':
@@ -24,3 +28,9 @@ def tweet(request):
         my_tweet.save()
         return redirect('/tweet')
 
+
+@login_required
+def delete_tweet(request, id):
+    my_tweet = TweetModel.objects.get(id = id)
+    my_tweet.delete()
+    return redirect('/tweet')
